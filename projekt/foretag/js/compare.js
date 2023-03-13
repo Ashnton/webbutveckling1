671a2d8856e50ec998a5a6f248e87ac10301c2f0
Ashnton <antonlidstrom71@gmail.com>
2023-03-13 14:49:03 +0100

    progress

--- projekt/foretag/js/compare.js
+++ projekt/foretag/js/compare.js
@@ -1,6 +1,13 @@
 document.getElementById("compare-btn").addEventListener("click", compareProducts);
 
 function compareProducts() {
+    const productDivs = document.getElementsByClassName("product-div");
+    for (let i = 0; i < productDivs.length; i++) {
+        const element = productDivs[i];
+        element.style.display = "none";    
+        console.log("1");
+    }
+
     const selectedProduct1 = document.getElementById("product-picker1").value;
     const selectedProduct2 = document.getElementById("product-picker2").value;
 
